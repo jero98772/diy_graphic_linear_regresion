a488bf996476a0bc2dc8f2be900ae853c0fec951
jero98772 <jero98772@gmail.com>
2019-06-15 14:17:15 -0500

    organize code

--- diy_linear regression.py
+++ diy_linear regression.py
@@ -2,10 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x=np.array([-2,-1,0,1,2])
+
 w =eval(input("W pending of line:\n"))
 b =eval(input("B intercept of line:\n"))
-#for x in x:
+
 y=w*x+b
+
 plt.plot(y ,linewidth=2.0)
 plt.ylabel('some numbers')
 plt.show()
