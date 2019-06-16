cc7482ec6a12623ce22962ec69c9beb4218fd171
jero98772 <jero98772@gmail.com>
2019-06-15 21:50:16 -0500

    add x points

--- diy_linear regression.py
+++ diy_linear regression.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x=np.array([-2,-1,0,1,2])
-
-w =eval(input("W pending of line:\n"))
-b =eval(input("B intercept of line:\n"))
+X=[
+8,8,7,8,7,8,8,8,8,8,8,7,8,8,
+7,8,9,7,8,7.2,7.2,7,4,7,6,7,8,7,7,7]#example data
+x=np.array(X)
+w =eval(input("W pending of line:\n"))#1
+b =eval(input("B intercept of line:\n"))#0
 
 y=w*x+b
+
+plt.plot(X ,'ro',linewidth=2.0)
 
 plt.plot(y ,linewidth=2.0)
 plt.ylabel('some numbers')
